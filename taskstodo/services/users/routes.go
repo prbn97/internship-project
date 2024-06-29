344500ec0d8c5257cd09c08041b05ff3f96c78ec
prbn97 <pauloreis.rj97@gmail.com>
2024-06-29 01:45:36 -0300

    feat: services types and util funcs

--- taskstodo/services/users/routes.go
+++ taskstodo/services/users/routes.go
@@ -1,6 +1,11 @@
 package users
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/prbn97/internship-project/types"
+	"github.com/prbn97/internship-project/utils"
+)
 
 type Handler struct {
 	// the Handler can take any dependence
@@ -11,5 +16,25 @@ func NewHandler() *Handler {
 }
 
 func (h *Handler) RegisterRoutes(serv *http.ServeMux) {
+	serv.HandleFunc("POST /users/login", h.handleLogin)
+	serv.HandleFunc("POST /users/register", h.handleRegister)
+
+}
+
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+	// recive payloas to get eamil, user  and password
+	// parse to json, validate
+	// check if the user exist
+
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+	// check if the user exists
+	// if [false] create a user
 
 }
--- taskstodo/types/types.go
+++ taskstodo/types/types.go
@@ -1,16 +1,58 @@
 package types
 
-// Users
+import "time"
 
-// Tasks
+// Users Payloads
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName" validate:"required"`
+	LastName  string `json:"lastName" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=3,max=130"`
+}
+
+type LoginUserPayload struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+// Users Object
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+// Users Interface
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
+
+// Tasks Payloads
 type TaskPayLoad struct {
 	Title       string `json:"title" validate:"required"`
 	Description string `json:"description"`
 }
 
+// Tasks Object
 type Task struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
+	ID          int       `json:"id"`
+	UserID      int       `json:"userID"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+}
+
+// Tasks Interface
+type TaskStore interface {
+	CreateTask(TaskPayLoad) error
+	ListTasks() ([]*Task, error)
+	GetTaskByID(id string) (*Task, error)
+	UpdateTask(Task) error
+	DeleteTask(id string) (Task, error)
 }
