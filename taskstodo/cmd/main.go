7807d4ee0c3b8fd068d65896414bb425d1ec04ea
prbn97 <pauloreis.rj97@gmail.com>
2024-07-17 14:10:57 -0300

    build: add environment variables by .env files

--- taskstodo/cmd/main.go
+++ taskstodo/cmd/main.go
@@ -6,23 +6,13 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/joho/godotenv"
 	"github.com/prbn97/internship-project/services/task"
 )
 
-func enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
+	loadEnvFiles()
+
 	// build storage
 	dbPath := filepath.Join("db", "tasksStore.json")
 	tasksDB, err := task.NewStore(dbPath)
@@ -45,5 +35,34 @@ func main() {
 	// start the server
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), corsHandler); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func loadEnvFiles() {
+
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println(".env file not found. Trying to load env.example")
+
+		if err := godotenv.Load("env.example"); err != nil {
+			log.Println("No .env or env.example files found")
+		} else {
+			log.Println("env.example file loaded")
+		}
+
+	} else {
+		log.Println(".env file loaded")
 	}
 }
