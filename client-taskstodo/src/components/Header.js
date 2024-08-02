e3db4a70723534dd0fa15d002ad33f78c4ac118c
prbn97 <pauloreis.rj97@gmail.com>
2024-08-02 16:55:28 -0300

    style: header component, expose tasksStore.json and update .env and docker-compose

--- client-taskstodo/src/components/Header.js
+++ client-taskstodo/src/components/Header.js
@@ -12,39 +12,39 @@ const Header = (props) => {
     }
 
 
-    return (<>
-        <br />
-        <Navbar className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand as={Link} to="/">
-                    <img
-                        alt="logo"
-                        src={logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    Tasks ToDo
-                </Navbar.Brand>
+    return (
+        <>
+            <Navbar className="bg-body-tertiary">
+                <Container >
+                    <Navbar.Brand as={Link} to="/">
+                        <img
+                            alt="logo"
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Tasks ToDo
+                    </Navbar.Brand>
 
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {/* using "as={Link} to=" to use React Router's Link*/}
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        {props.jwtToken ? (
-                            <>
-                                <Nav.Link as={Link} to="/tasks">Tasks</Nav.Link>
-                                <Nav.Link as={Link} to="/login" onClick={logOut}>Logout</Nav.Link>
-                            </>
-                        ) : (
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
+                    <Navbar.Collapse id="basic-navbar-nav" className="flex-row-reverse">
+                        <Nav>
+                            {/* using "as={Link} to=" to use React Router's Link*/}
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            {props.jwtToken ? (
+                                <>
+                                    <Nav.Link as={Link} to="/tasks">Tasks</Nav.Link>
+                                    <Nav.Link as={Link} to="/login" onClick={logOut}>Logout</Nav.Link>
+                                </>
+                            ) : (
+                                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
-            </Container>
-        </Navbar>
-    </>
+        </>
     );
 }
 
